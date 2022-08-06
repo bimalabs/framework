c4132022c13b0132ca44fc2a4be4bf210659aab8
Muhammad Surya Ikhsanuddin <surya.kejawen@gmail.com>
2022-08-06 13:06:09 +0700

    remove memory allocation 'in' health

--- routes/health.go
+++ routes/health.go
@@ -2,9 +2,6 @@ package routes
 
 import (
 	"net/http"
-	"runtime"
-	"strconv"
-	"strings"
 
 	"github.com/goccy/go-json"
 
@@ -34,36 +31,14 @@ func (h *Health) SetClient(client *grpc.ClientConn) {
 }
 
 func (h *Health) Handle(w http.ResponseWriter, r *http.Request, _ map[string]string) {
-	var m runtime.MemStats
-	var alloc strings.Builder
-	var totalAlloc strings.Builder
-	var system strings.Builder
-
-	runtime.ReadMemStats(&m)
-
-	alloc.WriteString(strconv.Itoa(h.byteToMb(m.Alloc)))
-	alloc.WriteString(" MiB")
-	totalAlloc.WriteString(strconv.Itoa(h.byteToMb(m.TotalAlloc)))
-	totalAlloc.WriteString(" MiB")
-	system.WriteString(strconv.Itoa(h.byteToMb(m.Sys)))
-	system.WriteString(" MiB")
 
 	payload := map[string]interface{}{
 		"version": bima.Version,
 		"name":    "Bima Framework",
 		"author":  "Muhamad Surya Iksanudin<surya.iksanudin@gmail.com>",
 		"link":    "https://github.com/bimalabs/framework",
-		"memory_usage": map[string]string{
-			"allocation":       alloc.String(),
-			"total_allocation": totalAlloc.String(),
-			"system":           system.String(),
-		},
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(payload)
-}
-
-func (h *Health) byteToMb(b uint64) int {
-	return int(b / 1024 / 1024)
 }
